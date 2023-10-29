fd6f6245b89447257d13b328262aa323fa1ccf86
Joao0028 <joaopaulonascimento720@gmail.com>
2023-10-29 13:21:50 -0300

    aprendendo a utilizar o lazy para carregamento de componentes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,22 @@
 import Footer from 'components/Footer';
 import Menu from 'components/Menu';
 import PaginaPadrao from 'components/PaginaPadrao';
-import Cardapio from 'pages/Cardapio';
+// import Cardapio from '';
 import Inicio from 'pages/Inicio';
 import NotFound from 'pages/NotFound';
 import Sobre from 'pages/Sobre';
 import Prato from 'pages/Prato';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const Cardapio = lazy(()=> import("pages/Cardapio"));
 
 export default function AppRouter() {
   return (
     <main className='container'>
       <Router>
-        <Menu />
+       <Suspense fallback={<>Carregando...</>}>
+       <Menu />
         <Routes>
           <Route path='/' element={<PaginaPadrao />} >
             <Route index element={<Inicio />} />
@@ -23,6 +27,7 @@ export default function AppRouter() {
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
+       </Suspense>
       </Router>
     </main>
   );
