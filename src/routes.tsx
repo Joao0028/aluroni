eeecfd598fd49e38b60ea7d19722f7a5fa12a3c8
Joao0028 <joaopaulonascimento720@gmail.com>
2023-10-29 13:29:07 -0300

    aplicando o React.lazy nos demais componentes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,19 @@
 import Footer from 'components/Footer';
 import Menu from 'components/Menu';
-import PaginaPadrao from 'components/PaginaPadrao';
+// import PaginaPadrao from 'components/PaginaPadrao';
 // import Cardapio from '';
-import Inicio from 'pages/Inicio';
-import NotFound from 'pages/NotFound';
-import Sobre from 'pages/Sobre';
+// import Inicio from 'pages/Inicio';
+// import NotFound from 'pages/NotFound';
+// import Sobre from 'pages/Sobre';
 import Prato from 'pages/Prato';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
 const Cardapio = lazy(()=> import("pages/Cardapio"));
+const PaginaPadrao = lazy(()=> import("components/PaginaPadrao"));
+const Inicio = lazy(()=> import("pages/Inicio"));
+const Sobre = lazy(()=> import("pages/Sobre"));
+const NotFound = lazy(()=> import("pages/NotFound"));
 
 export default function AppRouter() {
   return (
